f198c1b019cfcc5b0f1353cf840ee2608a49d616
Unknown <fauriciocr@gmail.com>
2018-06-10 16:26:13 -0600

    Revert "Merge branch 'master' into fauricio2.0"
    This reverts commit bfcf7192e54ca8c013811981b74e2ab2cc30f845.

--- android/src/Vista/Player/player_achiev_Fragment.java
+++ android/src/Vista/Player/player_achiev_Fragment.java
@@ -6,18 +6,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ListView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.mygdx.game.R;
 
 import org.json.JSONArray;
@@ -30,16 +24,12 @@ import Controlador.Achievement_adapter;
 import Controlador.Controller;
 import Modelo.Achievement;
 import Vista.Admin.add_achievement;
-import Vista.login;
 
 public class player_achiev_Fragment extends Fragment {
     private View rootview;
     private ListView achievement;
     private Achievement_adapter adapter;
     private ArrayList<Achievement> ArrayItem = null;
-    private EditText filterText;
-    private ImageButton exit;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,53 +50,6 @@ public class player_achiev_Fragment extends Fragment {
 
             }
         });
-
-        exit = rootview.findViewById(R.id.imageButton);
-
-        exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (FirebaseAuth.getInstance().getCurrentUser() != null){
-                    FirebaseAuth.getInstance().signOut();
-                }
-                Intent i = new Intent(getContext(), login.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
-            }
-        });
-
-        filterText = rootview.findViewById(R.id.filter);
-        filterText.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void afterTextChanged(Editable arg0) {
-                // TODO Auto-generated method stub
-                String text = filterText.getText().toString().toLowerCase();
-                adapter.filter(text);
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence arg0, int arg1,
-                                          int arg2, int arg3) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onTextChanged(CharSequence arg0, int arg1, int arg2,
-                                      int arg3) {
-                // TODO Auto-generated method stub
-            }
-        });
-        filterText.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                v.setFocusable(true);
-                v.setFocusableInTouchMode(true);
-                return false;
-            }
-        });
-
-
         cargarLista(rootview.getContext());
         return rootview;
     }
--- android/src/Vista/login.java
+++ android/src/Vista/login.java
@@ -4,31 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import Controlador.Controller;
 import Vista.Player.playerActivity;
 import com.mygdx.game.AndroidLauncher;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.mygdx.game.R;
 
 import org.json.JSONException;
@@ -39,65 +22,13 @@ import Vista.Admin.dashboard_admin;
 public class login extends AppCompatActivity {
     private EditText mail1,password1;
 
-
-    SignInButton button;
-    FirebaseAuth mAuth;
-    private final static int RC_SIGN_IN = 2;
-    GoogleApiClient mGoogleApiClient;
-    FirebaseAuth.AuthStateListener mAuthListener;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mail1 = findViewById(R.id.txt_email_login);
         password1 = findViewById(R.id.txt_password_login);
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser() != null){
-                    startActivity(new Intent(login.this,dashboard_admin.class));
-                }
-            }
-        };
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        Toast.makeText(login.this,"Something went wrong",Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-                .build();
-
-        button = (SignInButton) findViewById(R.id.googleBtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
-            }
-        });
-
-
     }
-
-
-
-
 
     public void logIn(View v){
         //String mail_aux = mail1.getText().toString();
@@ -146,58 +77,6 @@ public class login extends AppCompatActivity {
         //Intent  intent = new Intent(getApplicationContext(),playerActivity.class);
         Intent  intent = new Intent(getApplicationContext(),AndroidLauncher.class);
         startActivity(intent);
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Toast.makeText(login.this,"Auth went wrong",Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("TAG", "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //Se puede obtener el correo para saber si es o no admin y redirigir la app.
-                            //Log.d("TAG", user.getEmail() + user.getDisplayName());
-                            startActivity(new Intent(login.this,dashboard_admin.class));
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("TAG", "signInWithCredential:failure", task.getException());
-                            Toast.makeText(login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
     }
 }
 
